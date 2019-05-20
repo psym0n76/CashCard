a3ba80040590650ebe5d6654215f94f04ad09b22
Simon Burton <burt76@outlook.com>
2019-05-20 15:52:50 +0100

    global cash card working with tests

---
+++ CashCard.Library/CardGlobal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace CashCard.Library
+{
+    public class CardGlobal : ICash
+
+    {
+        private const int SecurityPin = 1234;
+        private double _balance;
+
+        private static readonly CardGlobal Instance = new CardGlobal();
+
+        public static CardGlobal GetInstance
+        {
+            get { return Instance; }
+        }
+
+        public double Balance
+        {
+            get
+            {
+                if (IsPinOkFunction())
+                    return _balance;
+
+                return 0;
+            }
+        }
+
+        public int Pin { get; set; }
+
+
+        public void IsPinOk()
+        {
+            if (Pin != SecurityPin)
+                throw new Exception("SecurityPin Invalid");
+        }
+
+        public bool IsPinOkFunction()
+        {
+            if (Pin != SecurityPin)
+            {
+                throw new Exception("SecurityPin Invalid");
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+
+        public void Deposit(double amount)
+        {
+            if (IsPinOkFunction())
+                _balance = Balance + amount;
+        }
+
+        public void Withdrawal(double amount)
+        {
+            if (IsPinOkFunction())
+                if (_balance < amount)
+                    throw new Exception("Not enough Cash in account");
+
+            _balance = Balance - amount;
+        }
+
+
+    }
+}
---
+++ CashCard.Tests/CashGlobal.Tests.cs
@@ -0,0 +1,104 @@
+﻿using CashCard.Library;
+using NUnit.Framework;
+
+namespace CashCard.Tests
+{
+
+    [TestFixture]
+    public class CashGlobalTests
+    {
+        private CardGlobal _cashCard;
+
+        [SetUp]
+        public void Setup()
+        {
+            _cashCard = CardGlobal.GetInstance;
+            _cashCard.Pin = 1234;
+        }
+
+
+        [Test]
+        public void Card_CheckPin_ExceptionThrownIfPinIsNotOk()
+        {
+            var cashCard = CardGlobal.GetInstance;
+            cashCard.Pin = 1;
+            Assert.That(() => cashCard.IsPinOk(), Throws.Exception);
+        }
+
+        [Test]
+        public void Card_CheckPin_NoExceptionThrownIfPinIsOk()
+        {
+            var cashCard = CardGlobal.GetInstance;
+            cashCard.Pin = 1234;
+            Assert.That(() => cashCard.IsPinOk(), Throws.Nothing);
+        }
+
+        [Test]
+        public void Card_Deposit_BalanceShouldIncreaseByMultipleInputs()
+        {
+            var cashCard = _cashCard;
+            var existingBalance = cashCard.Balance;
+
+            cashCard.Deposit(1);
+            cashCard.Deposit(1);
+
+            //assert
+            Assert.That(cashCard.Balance, Is.EqualTo((2 - existingBalance)));
+        }
+
+
+        [Test]
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        public void Card_Deposit_BalanceShouldIncreaseByTheInputAmount(int input, int expected)
+        {
+            //act
+            var cashCard = _cashCard;
+            var existingBalance = cashCard.Balance;
+            cashCard.Deposit(input);
+
+            //assert
+            Assert.That(cashCard.Balance-existingBalance, Is.EqualTo(expected));
+        }
+
+
+
+
+        [Test]
+        [TestCase(1, 9, 10)]
+        [TestCase(2, 8, 10)]
+        public void Card_Withdrawal_BalanceShouldDecreaseByTheInputAmount(int input, int expected, int initialBalance)
+        {
+            var cashCard = _cashCard;
+            var existingBalance = cashCard.Balance;
+            cashCard.Deposit(initialBalance);
+            cashCard.Withdrawal(input);
+            Assert.That(cashCard.Balance - existingBalance, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Card_Withdrawal_BalanceShouldDecreaseByMultipleInputs()
+        {
+            var cashCard = _cashCard;
+            var existingBalance = cashCard.Balance;
+            //act
+            cashCard.Deposit(20);
+            cashCard.Withdrawal(10);
+            cashCard.Withdrawal(10);
+
+            //assert
+            Assert.That(cashCard.Balance - existingBalance, Is.EqualTo(0));
+        }
+
+
+
+        [Test]
+        public void Card_Withdrawal_ExceptionThrownIfBalanceIsLessThanZero()
+        {
+            var cashCard = _cashCard;
+            Assert.That(() => cashCard.Withdrawal(100), Throws.Exception);
+        }
+
+
+    }
+}
