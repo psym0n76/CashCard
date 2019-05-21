9fa1c6551bcabaa0cfc9a66930c2b7ef4b263b58
Simon Burton <burt76@outlook.com>
2019-05-21 15:49:31 +0100

    added locking to Card class all tests passed

--- CashCard.Library/Card.cs
+++ CashCard.Library/Card.cs
@@ -9,6 +9,7 @@ namespace CashCard.Library
         private const int SecurityPin = 1234;
         private double _balance;
         private readonly int _pin;
+        private static readonly object _locker = new object();
 
         public Card(int pin)
         {
@@ -29,21 +30,30 @@ namespace CashCard.Library
 
         public void Deposit(double amount)
         {
-            IsPinOk();
-            _balance = Balance + amount;
+
+            lock (_locker)
+            {
+                IsPinOk();
+                _balance = Balance + amount;
+            }
         }
 
         public void Withdrawal(double amount)
         {
-            IsPinOk();
-            if (_balance < amount)
-                throw new Exception("Not enough Cash in account");
+            lock (_locker)
+            {
+                IsPinOk();
+                if (_balance < amount)
+                    throw new Exception("Not enough Cash in account");
 
-            _balance = Balance - amount;
+                _balance = Balance - amount;
+            }
         }
 
         public double Balance
         {
+
+            //add locking
             get
             {
                 IsPinOk();
